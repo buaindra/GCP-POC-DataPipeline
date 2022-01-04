92c60e466b99636c0aa70555500117c626f9a8ae
Indranil <indranilbua.1994.01@gmail.com>
2022-01-04 16:05:05 +0000

    added onetime stratup script

--- README.md
+++ README.md
@@ -8,3 +8,38 @@
 > Using **Dataflow**, join data between Pubsub (streaming) and Cloud Datastore (Static Data) and do some transformation and load into Bigquery
 
 > Use **BigQuery** for data warehouse
+
+### Step1: CLone the code from GIT Repo
+> git clone git@github.com:buaindra/GCP-POC-DataPipeline.git
+> cd GCP-POC-DataPipeline
+> git pull origin main
+
+### Step2: Execute the Onetime Startup Script
+> bash **Onetime_Env.sh**
+
+### Step3: Create Python Virtual Environment and install the Apache Beam SDKs
+> python3 -m virtualenv env
+> source env/bin/activate
+
+> pip install -m 'apache-beam[gcp]'
+
+### Step4: Run the Beam Pipeline Locally for testing
+> python hello-beam.py --project $PROJECT_ID --topic $PUBSUB_TOPIC --output beam.out --runner DirectRunner
+
+### Step5: Then run the pipeline in dataflow runner
+> python -m apache_beam.examples.wordcount \
+> --region $REGION \
+> --input gs://dataflow-samples/shakespeare/kinglear.txt \
+> --output gs://$GCS_BUCKET_01/results/outputs \
+> --runner DataflowRunner \
+> --project $PROJECT_ID \
+> --temp_location gs://$GCS_BUCKET_01/tmp/ 
+
+### Step6: Verify the output
+> # List the output files
+> gsutil ls -lh "gs://$GCS_BUCKET_01/results/outputs*"  
+> 
+> # View the results in the output files:
+> gsutil cat "gs://$GCS_BUCKET_01/results/outputs*"
+
+
---
+++ bash_scripts/onetime_startup_script.sh
@@ -0,0 +1,57 @@
+# Environment Variable
+export PROJECT_ID=indranil-24012994-01 \
+export BILLING_ACCOUNT_ID=01F748-D68B6C-7BFEF3 \
+export SERVICE_ACCOUNT_ID=sa-composer-dataflow \
+export REGION=us-central1 \
+export GCS_BUCKET_01="gcs-$PROJECT_ID" \
+export PUBSUB_TOPIC=pubsub-topic-poc-01 \
+export PUBSUB_SUBSCRIPTION_01=pubsub-subscription-poc-01 \
+export BIGQUERY_DATASET=poc_dataflow \
+export BIGQUERY_TABLE_01=detailed_view \
+
+echo "Variable Setup Done"
+
+# Create the Project
+gcloud projects create $PROJECT_ID --name 'Composer Dataflow POC'
+echo "New Project has been created"
+
+# Enable billing for newly created project
+gcloud alpha billing projects link $PROJECT_ID --billing-account $BILLING_ACCOUNT_ID
+echo "Billing account has been assigned to the project"
+
+# Set to the newly created Project
+gcloud config set project $PROJECT_ID
+echo "set the project"
+
+# Enable the API
+gcloud services enable dataflow.googleapis.com
+gcloud services enable pubsub.googleapis.com
+echo "APIs has been enabled"
+
+# create the service account
+gcloud iam service-accounts create $SERVICE_ACCOUNT_ID \
+--description='service account for POC' \
+--display-name='sa-composer-dataflow'
+echo "Service Account has been created"
+
+# assign the role to that specific service account
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+--member="serviceAccount:$SERVICE_ACCOUNT_ID@$PROJECT_ID.iam.gserviceaccount.com" \
+--role="roles/editor"
+echo "role has been assigned to that service account"
+
+# Create GCS Bucket
+gsutil mb -c standard -b off -l us-central1 gs://$GCS_BUCKET_01
+echo "GCS Bucket has been created"
+
+# Create Pub/Sub Topic
+gcloud pubsub topics create $PUBSUB_TOPIC --project $PROJECT_ID
+echo "pubsub topic has been created"
+
+# Bigquery Dataset Creation
+bq mk --location us --description 'for demo poc' --dataset $PROJECT_ID:$BIGQUERY_DATASET
+echo "Bigquery Dataset has been created"
+
+# Bigquery Table Creation
+bq mk --location us-central1 --table $PROJECT_ID:$BIGQUERY_DATASET.$BIGQUERY_TABLE_01
+
